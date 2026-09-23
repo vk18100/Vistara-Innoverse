f14f1ec14b0b393ba391d9cede2fabc033fe338f
Singh vishal <sv1800000@gmail.com>
2026-09-23 11:41:58 +0530

    feat: configure CORS for ML API

--- ml/api/main.py
+++ ml/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from ml.recommendation.recommender import (
@@ -41,6 +42,18 @@ class VerificationRequest(BaseModel):
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
